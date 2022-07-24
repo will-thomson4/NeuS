2c3407b56c479ac04ba770cc895aa5def0a8806a
Will <will.thomson4@gmail.com>
2022-07-24 17:37:06 +0100

    path changes

--- preprocess_custom_data/colmap_preprocess/colmap_wrapper.py
+++ preprocess_custom_data/colmap_preprocess/colmap_wrapper.py
@@ -26,7 +26,7 @@ def run_colmap(basedir, match_type):
     logfile = open(logfile_name, 'w')
     
     feature_extractor_args = [
-        '/data/wt21/colmap', 'feature_extractor', 
+        '/data/wt21/colmap/bin/colmap', 'feature_extractor', 
             '--database_path', os.path.join(basedir, 'database.db'), 
             '--image_path', os.path.join(basedir, 'images'),
             '--ImageReader.single_camera', '1',
@@ -37,7 +37,7 @@ def run_colmap(basedir, match_type):
     print('Features extracted')
 
     exhaustive_matcher_args = [
-        '/data/wt21/colmap', match_type, 
+        '/data/wt21/colmap/bin/colmap', match_type, 
             '--database_path', os.path.join(basedir, 'database.db'), 
     ]
 
@@ -58,7 +58,7 @@ def run_colmap(basedir, match_type):
     #         '--Mapper.init_min_tri_angle', '4',
     # ]
     mapper_args = [
-        '/data/wt21/colmap', 'mapper',
+        '/data/wt21/colmap/bin/colmap', 'mapper',
             '--database_path', os.path.join(basedir, 'database.db'),
             '--image_path', os.path.join(basedir, 'images'),
             '--output_path', os.path.join(basedir, 'sparse'), # --export_path changed to --output_path in colmap 3.6
