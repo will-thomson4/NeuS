f296a752924b3b691c65251bc6a96dd77cda81f1
Will <will.thomson4@gmail.com>
2022-07-26 17:32:56 +0100

    debg ug

--- models/dataset.py
+++ models/dataset.py
@@ -113,10 +113,13 @@ class Dataset:
         """
         Generate random rays at world space from one camera.
         """
-        print(len(self.masks), img_idx)
         pixels_x = torch.randint(low=0, high=self.W, size=[batch_size])
         pixels_y = torch.randint(low=0, high=self.H, size=[batch_size])
-        color = self.images[img_idx][(pixels_y, pixels_x)]    # batch_size, 3
+        color = self.images[img_idx][(pixels_y, pixels_x)]
+        
+        print("Mask length: ", len(self.masks[img_idx]))
+        print("Pixels: ", (pixels_y, pixels_x))
+            # batch_size, 3
         mask = self.masks[img_idx][(pixels_y, pixels_x)]      # batch_size, 3
         p = torch.stack([pixels_x, pixels_y, torch.ones_like(pixels_y)], dim=-1).float()  # batch_size, 3
         p = torch.matmul(self.intrinsics_all_inv[img_idx, None, :3, :3], p[:, :, None]).squeeze() # batch_size, 3
