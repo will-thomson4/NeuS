6b0529b89bb145b9d66c7e98150473355f584493
Will <will.thomson4@gmail.com>
2022-08-23 11:46:17 +0100

    added depth term back in

--- models/dataset.py
+++ models/dataset.py
@@ -186,23 +186,23 @@ class Dataset:
         mid = 0.5 * (-b) / a
 
         #Use depth as mid
-        # if depth is not None:
-        #     depth = torch.sum(depth, dim=-1, keepdim=True)            
-        #     mean = torch.mean(depth)
-        #     std = torch.std(depth)
-        #     depth = (depth-mean)/std
-        #     depth = depth * 0.17 + 1.75
+        if depth is not None:
+            depth = torch.sum(depth, dim=-1, keepdim=True)            
+            mean = torch.mean(depth)
+            std = torch.std(depth)
+            depth = (depth-mean)/std
+            depth = depth * 0.17 + 1.75
 
-        #     # #Save a plot of mid data
-        #     # plt.plot(mid.cpu().numpy()[:50,0])
-        #     # #save a plot of depth data
-        #     # plt.plot(depth.cpu().numpy()[:50,0])
-        #     # #save the plot to a file
-        #     # print("Saving plot to file")
-        #     # plt.savefig("depth2.png")
-        #     # plt.clf()
+            # #Save a plot of mid data
+            # plt.plot(mid.cpu().numpy()[:50,0])
+            # #save a plot of depth data
+            # plt.plot(depth.cpu().numpy()[:50,0])
+            # #save the plot to a file
+            # print("Saving plot to file")
+            # plt.savefig("depth2.png")
+            # plt.clf()
 
-        #     mid = depth
+            mid = depth
 
         near = mid - 1
         far = mid + 1
